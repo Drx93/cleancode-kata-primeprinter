0d0c123858a017da675bc005819072b19934e84f
Drx93 <dorian93.roux@gmail.com>
2024-12-13 10:55:24 +0100

    Exercise3 ajout

---
+++ start/cleancode-kata-primeprinter/src/main/java/Exercise3.java
@@ -0,0 +1,29 @@
+public class Exercise3 {
+    /**
+     *
+     * @param amount price
+     * @param type Account status (NotRegistered = 1, SimpleCustomer = 2, ValuableCustomer = 3, MostValuableCustomer = 4)
+     * @param years Time in years that the client is our customer
+     * @return THE PRICE
+     */
+    public double calculate(double amount, int type, int years) {
+        double discount = (years > 5) ? 0.05 : years / 100.0;
+        double result;
+
+        switch (type) {
+            case 1: // NotRegistered
+                return amount;
+            case 2: // SimpleCustomer
+                result = amount - 0.1 * amount;
+                return result - discount * result;
+            case 3: // ValuableCustomer
+                result = 0.7 * amount;
+                return result - discount * result;
+            case 4: // MostValuableCustomer
+                result = amount - 0.5 * amount;
+                return result - discount * result;
+            default:
+                return amount; // In case of invalid type
+        }
+    }
+}
